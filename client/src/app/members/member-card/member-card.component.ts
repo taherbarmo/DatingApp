f335ff787c88e5f0ef10db244795d75c63d52f2b
Barmo <Taher.Barmo@lachmann-rink.de>
2023-06-12 16:35:52 +0200

    section14

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,7 @@
 import { Component ,Input} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_service/members.service';
 
 
 @Component({
@@ -10,4 +12,12 @@ import { Member } from 'src/app/_models/member';
 export class MemberCardComponent {
 
   @Input() member : Member | undefined;
+
+  constructor(private memberService: MembersService, private toastr: ToastrService) { }
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.userName).subscribe({
+      next: () => this.toastr.success('You have liked ' + member.knownAs)
+    })
+  }
 }
